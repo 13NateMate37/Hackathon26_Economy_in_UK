fd57a71b21974ad34f019b7c40da2c9db26f78e7
Nathan <NateMateX2Z@outlook.com>
2026-09-14 16:03:53 +0100

    Streamlit prep

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="You wanna live where?",
+    page_icon="🌍",
+    layout="wide"
+    )
+
+st.title("Western Europe's Affordability Comparison")
+
+st.write(
+    """
+    This dashabord explores the balance of affordability in Western Europe,
+      aiming to expand globally, section by section. View how much countries 
+      cost to live in against the purchasing power they provide you.  
+    """
+)
+
